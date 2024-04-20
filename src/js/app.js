ef801f07f2a2957989847611e9d8e79570ec2716
MyCode07 <erem19992015@mail.ru>
2024-04-20 15:59:47 +0300

    add some changes

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ import { maskInputs } from "./static/inputmask.js";
 import { replaceDomElements } from "./static/replace.js";
 import { animateAction, animateStaggerAction } from "./parts/animations.js";
 
-import "./parts/input-hover.js";
 import "./parts/popup.js";
 import "./parts/menu.js";
 import "./parts/more-photos.js";
@@ -16,6 +15,23 @@ maskInputs('+7 (999) 999-99-99', '._mask-phone')
 import { Fancybox } from "@fancyapps/ui";
 Fancybox.bind("[data-fancybox]", {
 });
+
+const inputItems = [...document.querySelectorAll('input ')].concat([...document.querySelectorAll('textarea ')])
+if (inputItems.length) {
+    inputItems.forEach(input => {
+        if (input.closest('.form__item')) {
+            input.addEventListener('input', () => {
+                console.log(input.value);
+                if (input.value != '') {
+                    input.classList.add('_active')
+                }
+                else {
+                    input.classList.remove('_active')
+                }
+            })
+        }
+    })
+}
 
 
 document.addEventListener('click', function (e) {
